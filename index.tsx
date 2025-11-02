ec780d58646abcdc705eb215992e222fd45938ef
Subho-29X <Subhojit29ghosh@gmail.com>
2025-11-02 23:54:14 +0530

    chore: add startup logs to index.tsx

--- index.tsx
+++ index.tsx
@@ -4,10 +4,12 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to..");
+  throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+console.log("Starting Sorting Algorithm Visualizer");
+console.log("Mounting to element:", rootElement);
 root.render(
   <React.StrictMode>
     <App />
